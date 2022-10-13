dc240982f0c1c8db84222456551fcaaae5187e9a
VicN <vnikolaeff989@gmail.com>
2022-10-13 22:52:15 +0300

    Project init

---
+++ src/main/java/ru/geekbrains/habr/HabrApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.habr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/habr/HabrApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.habr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
