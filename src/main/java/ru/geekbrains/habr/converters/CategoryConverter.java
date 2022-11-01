476fcded4f34684ded3720d069b235c228046e1c
viktor-n <vnikolaeff989@gmail.com>
2022-11-01 12:40:16 +0300

    Добавил конвертер категорий

---
+++ src/main/java/ru/geekbrains/habr/converters/CategoryConverter.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.habr.converters;
+
+import org.springframework.stereotype.Component;
+import ru.geekbrains.habr.dtos.CategoryDto;
+import ru.geekbrains.habr.entities.Category;
+
+@Component
+public class CategoryConverter {
+
+    public CategoryDto entityToDto(Category category) {
+        return new CategoryDto(category.getId(), category.getName());
+    }
+}
