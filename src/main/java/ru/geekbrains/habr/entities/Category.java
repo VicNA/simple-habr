cae746a9e9543ff0c53209aee038261b484932e4
viktor-n <vnikolaeff989@gmail.com>
2022-11-01 12:36:43 +0300

    Фикс имени таблицы в сущности

--- src/main/java/ru/geekbrains/habr/entities/Category.java
+++ src/main/java/ru/geekbrains/habr/entities/Category.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 
 @Entity
 @Data
-@Table(name = "category")
+@Table(name = "categories")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
