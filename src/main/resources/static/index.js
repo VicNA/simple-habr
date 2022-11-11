6dc55593cf262b7567b0682d70ea7c07d7217553
Medvedev Maxim <maxie81@yandex.ru>
2022-11-11 13:41:04 +0300

    Sprint2. Front. Перевод списка Категорий на динамический список.

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,67 +1,84 @@
-(function () {
-    angular
-        .module('HabrApp', ['ngRoute', 'ngStorage'])
-        .config(config)
-        .run(run);
+(
+    function () {
+        angular
+            .module(
+                'HabrApp',
+                [
+                    'ngRoute',
+                    'ngStorage'
+                ]
+            )
+            .config(config)
+            .run(run);
 
-    function config($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'main/main.html',
-                controller: 'mainController'
-            })
-            .when('/profile', {
-                templateUrl: 'profile/profile.html',
-                controller: 'profileController'
-            })
-            .when('/authorization', {
-                            templateUrl: 'authorization/authorization.html',
-                            controller: 'authorizationController'
-                        })
-            .when('/design', {
-                            templateUrl: 'design/design.html',
-                            controller: 'designController'
-                        })
-            .when('/marketing', {
-                            templateUrl: 'marketing/marketing.html',
-                            controller: 'marketingController'
-                        })
-            .when('/mobile_dev', {
-                            templateUrl: 'mobile_dev/mobile_dev.html',
-                            controller: 'mobile_devController'
-                        })
-            .when('/web_dev', {
-                            templateUrl: 'web_dev/web_dev.html',
-                            controller: 'web_devController'
-                        })
-            .otherwise({
-                redirectTo: '/'
-            });
+        function config($routeProvider) {
+            $routeProvider
+                .when(
+                    '/',
+                    {
+                        templateUrl: 'main/main.html',
+                        controller: 'mainController'
+                    }
+                )
+                .when(
+                    '/profile',
+                    {
+                        templateUrl: 'profile/profile.html',
+                        controller: 'profileController'
+                    }
+                )
+                .when(
+                    '/authorization',
+                    {
+                        templateUrl: 'authorization/authorization.html',
+                        controller: 'authorizationController'
+                    }
+                )
+                .otherwise(
+                    {
+                        redirectTo: '/'
+                    }
+                )
+            ;
+        }
+
+        function run($rootScope, $http, $localStorage) {}
     }
+) ()
+;
 
-    function run($rootScope, $http, $localStorage) {
+angular
+    .module('HabrApp')
+    .controller(
+        'indexController',
+        function (
+            $rootScope,
+            $scope,
+            $http,
+            $location,
+            $localStorage
+        ) {
+            const contextPath = 'http://localhost:8189/habr/';
+            const categoryPath = 'api/v1/categories';
 
-       }
-})();
+            $rootScope.category = {name: "Все категории"};
 
+            $scope.getAllCategories = function () {
+                $http
+                    .get(contextPath + categoryPath)
+                    .then(
+                        function (response) {
+                            $scope.categories = response.data;
+                        }
+                    )
+                ;
+            }
 
-angular.module('HabrApp').controller('indexController', function ($rootScope, $scope, $http, $location, $localStorage) {
-   const contextPath = 'http://localhost:8189/habr/';
-   $scope.category = {name: "Все категории"};
+            $scope.setCategory = function (index) {
+                 $rootScope.category = $scope.categories[index];
+            }
 
-   $scope.getAllCategories = function () {
-                   $http
-                       .get(contextPath + 'api/v1/categories')
-                       .then(
-                           function (response) {
-                               $scope.categories = response.data;
-                           }
-                       );
-   }
-
-   $scope.setCategory = function (index) {
-                 $scope.category = $scope.categories[index];
-   }
-
-   $scope.getAllCategories();
-});
+            $scope.getAllCategories();
+        }
+    )
+;
