0823cea6995011b752bc3e2540a57c251b97e9e1
Medvedev Maxim <maxie81@yandex.ru>
2022-11-26 11:10:42 +0300

    Sprint4. Получение содержимого статьи через передачу ее ID в URL запроса("путь значений" PathVariable).

--- src/main/resources/static/article/article.js
+++ src/main/resources/static/article/article.js
@@ -9,17 +9,43 @@ angular
             $location,
             $localStorage
         ) {
-            const rootPath = 'http://localhost:8189/habr/';
+            const rootPath = 'http://' + window.location.host + '/habr/';
+            const articlesPath = 'api/v1/articles';
             const likesPath = 'api/v1/likes';
+            const defaultArticle =
+                {
+                    "id": -1,
+                    "title": "Все статьи",
+                    "text": ""
+                }
+            ;
             var path;
             var like;
+
+            $scope.getArticle = function (articleId) {
+                if (articleId == -1) {
+                    $scope.article = defaultArticle;
+                }
+
+                path = rootPath + articlesPath + "/view" + "/" +articleId;
+                $http
+                    .get(
+                        path
+                    )
+                    .then(
+                        function (response) {
+                            $scope.article = response.data;
+                        }
+                    )
+                ;
+            }
 
             $scope.addLike = function () {
                 path = rootPath + likesPath;
                 like =
                     {
                         "username": $localStorage.localUser.username,
-                        "articleId": $rootScope.article.id
+                        "articleId": $scope.article.id
                     };
 
                 $http
@@ -29,6 +55,8 @@ angular
                     )
                 ;
             }
+
+            $scope.getArticle($rootScope.articleId);
         }
     )
 ;
