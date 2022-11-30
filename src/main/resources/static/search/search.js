5756002970a906663d64c974bc886f26f6b946c0
viktor-n <vnikolaeff989@gmail.com>
2022-11-30 17:11:31 +0300

    Добавил страницу html и javascript для страницы Поиска, добавил routing в файлы index'а

---
+++ src/main/resources/static/search/search.js
@@ -0,0 +1,44 @@
+angular.module('HabrApp').controller('searchController', function($rootScope, $scope, $http, $localStorage) {
+    const contextPath = 'http://' + window.location.host + '/habr/api/v1/articles';
+
+    $scope.loadArticles = function () {
+//        if (pageIndex != $scope.currentPage) {
+//            $scope.currentPage = pageIndex;
+//        }
+
+        var title = document.getElementById('inputTitleSearch').value
+        var sort = document.getElementById('selectSearchSort').value +
+            ',' + document.querySelector('input[name="radioSort"]:checked').value
+
+        $http({
+            url: contextPath,
+            method: 'GET',
+            params: {
+                page: pageIndex,
+                title: title,
+                sort: sort
+            }
+        }).then(function(response) {
+               $scope.articles = response.data;
+           });
+    }
+
+//    $scope.updateStatus = function(articleId, statusName) {
+//        $http({
+//            url: contextPath + '/moderation/' + articleId +  '/updateStatus',
+//            method: 'PUT',
+//            params: {
+//                status: statusName
+//            }
+//        }).then(function(response) {
+//            $scope.getArticlesModeration();
+//        });
+//    }
+//
+//    $scope.setArticle = function(index) {
+//        console.log('index: ' + index);
+//        $rootScope.article = $scope.articlesModeration[index];
+//    }
+//
+    $scope.loadArticles();
+});
