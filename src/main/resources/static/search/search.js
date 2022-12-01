e040adf19c6f32da0b62714b3c58ce772114de1c
viktor-n <vnikolaeff989@gmail.com>
2022-12-01 17:17:36 +0300

    Добавил функции в search.js и применил изменения к search.html

--- src/main/resources/static/search/search.js
+++ src/main/resources/static/search/search.js
@@ -1,16 +1,21 @@
 angular.module('HabrApp').controller('searchController', function($rootScope, $scope, $http, $localStorage) {
     const contextPath = 'http://' + window.location.host + '/habr/api/v1/articles';
 
-    $scope.loadArticles = function () {
-//        if (pageIndex != $scope.currentPage) {
-//            $scope.currentPage = pageIndex;
-//        }
+    $scope.currentPage = 1;
+    totalPages = 1;
+
+    $scope.loadArticles = function (pageIndex = 1) {
+        if (pageIndex != $scope.currentPage) {
+            $scope.currentPage = pageIndex;
+        }
 
         var title = document.getElementById('inputTitleSearch').value;
 
         if (title.length > 0) {
-            var sort = document.getElementById('selectSearchSort').value +
-                ',' + document.querySelector('input[name="radioSort"]:checked').value;
+            var sort = [
+                document.getElementById('selectSearchSort').value,
+                document.querySelector('input[name="radioSort"]:checked').value
+            ].join(',');
 
             $http({
                 url: contextPath,
@@ -21,29 +26,18 @@ angular.module('HabrApp').controller('searchController', function($rootScope, $s
                     sort: sort
                 }
             }).then(function(response) {
-                   $scope.articles = response.data;
+                $scope.articles = response.data.content;
+                totalPages = response.data.totalPages;
+                $scope.paginationArray = Array.from({length: totalPages}, (x, i) => i + 1);
             });
         } else {
-            $scope.articles = []
+            $scope.articles = [];
         }
     }
 
-//    $scope.updateStatus = function(articleId, statusName) {
-//        $http({
-//            url: contextPath + '/moderation/' + articleId +  '/updateStatus',
-//            method: 'PUT',
-//            params: {
-//                status: statusName
-//            }
-//        }).then(function(response) {
-//            $scope.getArticlesModeration();
-//        });
-//    }
-//
-//    $scope.setArticle = function(index) {
-//        console.log('index: ' + index);
-//        $rootScope.article = $scope.articlesModeration[index];
-//    }
-//
-    $scope.loadArticles();
+    $scope.isPreviousPage = () =>  $scope.currentPage == 1 ? false : true;
+
+    $scope.isNextPage = () => ($scope.currentPage == totalPages) ? false : true;
+
+    $scope.loadArticles($scope.currentPage);
 });
