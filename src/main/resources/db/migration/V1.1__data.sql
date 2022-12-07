9e53c46a8c2b683d25bf5fd8861df804bb94e1cc
Tania <kovalenko2501@ya.ru>
2022-12-07 23:24:08 +0300

    добавлены картиночки

--- src/main/resources/db/migration/V1.1__data.sql
+++ src/main/resources/db/migration/V1.1__data.sql
@@ -18,24 +18,74 @@ values  (1, 2),
 insert into statuses(status_name)
 values ('hidden'), ('moderating'), ('published');
 
-insert into articles(user_id,dt_created,dt_published,title,text,status_id)
+insert into articles(user_id,dt_created,dt_published,title,text,status_id, image_path)
 select
     (select min(user_id) from users) as user_id,
     now()-1 as dt_created,
     now() as dt_published,
-    'Статья про UX-дизайн мобильных приложений и его влияние на продажи' as title,
-    'Aenean sit amet feugiat urna, ac luctus sapien. Etiam scelerisque ultricies sapien varius faucibus. Proin consectetur scelerisque nulla, ut efficitur felis rutrum eget. Nulla hendrerit sagittis egestas. Etiam rutrum fringilla orci, et dapibus ipsum mollis vel. Vestibulum hendrerit massa eget massa sollicitudin, egestas convallis tortor fermentum. Mauris at mauris vitae libero ultrices consequat. Ut blandit nisi felis, sed maximus mauris hendrerit sit amet. Curabitur facilisis neque et rutrum semper. Fusce id laoreet tortor. Nullam ornare, mauris iaculis feugiat laoreet, elit sem gravida urna, ut ullamcorper mauris dui eget justo. Aliquam consequat augue ut pellentesque hendrerit. Proin imperdiet dolor eget interdum posuere. Curabitur mi augue, interdum vitae fermentum sed, tristique a ligula. Donec lorem eros, cursus sit amet leo at, commodo vestibulum neque.' as text,
-    (select status_id from statuses where status_name = 'published') as status_id;
+    'Многопоточность в Java' as title,
+    'В этой статье я вкратце расскажу вам о процессах, потоках, и об основах многопоточного программирования на языке Java.
+     
+Наиболее очевидная область применения многопоточности – это программирование интерфейсов. ' ||
+    'Многопоточность незаменима тогда, когда необходимо, чтобы графический интерфейс продолжал отзываться на действия пользователя во время выполнения некоторой обработки информации. ' ||
+    'Например, поток, отвечающий за интерфейс, может ждать завершения другого потока, загружающего файл из интернета, и в это время выводить некоторую анимацию или обновлять прогресс-бар.' ||
+    'Кроме того он может остановить поток загружающий файл, если была нажата кнопка «отмена».
+    
+Еще одна популярная и, пожалуй, одна из самых хардкорных областей применения многопоточности – игры. ' ||
+    'В играх различные потоки могут отвечать за работу с сетью, анимацию, расчет физики и т.п.' as text,
+    (select status_id from statuses where status_name = 'published') as status_id,
+    'src/main/resources/files/java-tutorials.png' as image_path;
 
 insert into articles(user_id,dt_created,dt_published,title,text,status_id, image_path)
 select
     (select max(user_id) from users) as user_id,
     now()-1 as dt_created,
     now() as dt_published,
-    'Статья про проектирование эргономики НЛО' as title,
-    'Aenean sit amet feugiat urna, ac luctus sapien. Etiam scelerisque ultricies sapien varius faucibus. Proin consectetur scelerisque nulla, ut efficitur felis rutrum eget. Nulla hendrerit sagittis egestas. Etiam rutrum fringilla orci, et dapibus ipsum mollis vel. Vestibulum hendrerit massa eget massa sollicitudin, egestas convallis tortor fermentum. Mauris at mauris vitae libero ultrices consequat. Ut blandit nisi felis, sed maximus mauris hendrerit sit amet. Curabitur facilisis neque et rutrum semper. Fusce id laoreet tortor. Nullam ornare, mauris iaculis feugiat laoreet, elit sem gravida urna, ut ullamcorper mauris dui eget justo. Aliquam consequat augue ut pellentesque hendrerit. Proin imperdiet dolor eget interdum posuere. Curabitur mi augue, interdum vitae fermentum sed, tristique a ligula. Donec lorem eros, cursus sit amet leo at, commodo vestibulum neque.' as text,
+    'Лучшие практики Android-разработки 2022 за 1 минуту' as title,
+    '
+В этой статье мы кратко рассмотрим несколько наиболее важных советов, которые сделают вашу повседневную разработку приложений эффективной и легкой.
+ 
+1. Используйте Kotlin — Kotlin является предпочтительным языком для Android-разработки в 2022 году. С момента своего создания Kotlin много развивался и теперь стал зрелым языком.
+
+2. Используйте Jetpack Compose. Декларативная среда создания UI изменила представление о разработке для Android. Просто опишите свой пользовательский интерфейс, и фреймворк позаботится о нем, или, что проще, просто скажите, что вы хотите, и он покажет, что нужно.
+
+3. Выбирайте корутины Kotlin вместо RxJava — это простой и эффективный способ управления потоками.
+
+4. Используйте Hilt для внедрения зависимостей. Он сокращает количество шаблонного кода и позволяет отказаться от ручного внедрения зависимостей в вашем проекте.
+
+5. Используйте Room для локальной базы данных — лучший подход к сохранению данных, чем SQLiteDatabase. Он обеспечивает уровень абстракции над SQLite.
+
+6. Используйте Datastore для хранения пар «ключ-значение». Больше не используйте SharedPreferences! Сохраняйте данные асинхронно, согласованно и транзакционно, преодолевая недостатки SharedPreferences.
+
+7. Используйте Retrofit для работы с сетью — типобезопасный rest клиент для Java и Kotlin.
+
+8. Используйте WorkManager для планирования асинхронных задач — идеальное решение для планирования однократного или многократного запуска задач, даже если приложение закрывается или устройство перезагружается.
+
+9. Всегда измеряйте покрытие кода — JaCoco обладает мощными возможностями для измерения покрытия кода.
+
+10. Тестируйте свои классы с помощью модульных тестов. Вы уже знаете о важности написания модульных тестов. Используйте mockito-kotlin и библиотеку junit, чтобы легко писать тесты. Кроме того, используйте внедрение зависимостей, если это еще не сделано, чтобы разрешить использование классов-заглушек во время тестов.
+
+11. Тестируйте свой UI с помощью Espresso — Android позволяет очень легко писать тесты пользовательского интерфейса. Добавьте их и также запустите на CI.
+
+12. Используйте Coil для загрузки изображений. Знаменитая библиотека загрузки изображений для Android поддерживается корутины Kotlin.
+
+13. Используйте CI/CD для автоматизации разработки — CI/CD необходим для автоматизации процесса разработки. Автоматизируйте все ручные задачи и не беспокойтесь больше о них.
+
+14. Используйте Timber для ведения логов.
+
+15. Используйте Firebase Crashlytics для аналитики сбоев. Этот легкий инструмент для создания отчетов о сбоях в режиме реального времени и он упрощает управление стабильностью вашего приложения.
+
+16. Используйте ktLint для форматирования кода — линтер Kotlin, защищающий от тривиальных проблем, со встроенным форматированием.
+
+17. Используйте архитектуру MVVM — Model-View-ViewModel — это идеальное решение для всех недостатков MVP и MVC, которое делает код тестируемым, несвязанным и простым в обслуживании.
+
+18. Модуляризируйте ваше приложение по функциям — это позволит добиться более быстрой сборки, сокращения boilerplate кода, меньших по размеру и удобных в сопровождении модулей, меньших циклических зависимостей, более простой навигации, разделения ответственности и многих других преимущества.
+
+19. Сделайте безопасность приложения приоритетной. Делая ваше приложение более безопасным, вы помогаете сохранить доверие пользователей и целостность устройства. Ознакомьтесь с этими рекомендациями по обеспечению безопасности, которые оказывают существенное положительное влияние на безопасность вашего приложения.
+' as text,
     (select status_id from statuses where status_name = 'published') as status_id,
-    'src/main/resources/files/qbfLJ9Cph_I.jpg' as image_path;
+    'src/main/resources/files/laptop-with-hands.jpg' as image_path;
+
 
 insert into categories(category_name, category_name_cyr)
 values
@@ -44,9 +94,3 @@ values
     ('Mobile dev', 'Мобильная разработка'),
     ('Web dev', 'Web разработка');
 
-insert into article_to_category(category_id, article_id)
-select
-    category_id,
-    (select article_id from articles where title = 'Статья про UX-дизайн мобильных приложений и его влияние на продажи' )
-from categories
-where category_name in ('Marketing', 'Design',  'Mobile dev');
